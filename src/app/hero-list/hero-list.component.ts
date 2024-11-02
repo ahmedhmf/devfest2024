bf843eb53a4d5afc59726012cdd008cd5f82dbe4
Ahmed Moustafa <ahmed.moustafa.ext@siemens-healthineers.com>
2024-11-02 17:11:44 +0100

    finished

--- src/app/hero-list/hero-list.component.ts
+++ src/app/hero-list/hero-list.component.ts
@@ -1,8 +1,9 @@
-import {Component, viewChild} from '@angular/core';
+import {Component, computed, signal, viewChild} from '@angular/core';
 import {superHeros} from "../superheros";
 import {SearchComponent} from "./search/search.component";
 import {JsonPipe, NgOptimizedImage} from "@angular/common";
 import {RouterLink} from "@angular/router";
+import {SuperHeroModel} from "../models/super-hero.model";
 
 @Component({
   selector: 'app-hero-list',
@@ -17,64 +18,68 @@ import {RouterLink} from "@angular/router";
   styleUrl: './hero-list.component.css'
 })
 export class HeroListComponent {
-  superHeroList = superHeros;
-  filteredList = superHeros;
-  herosNumber = superHeros.length;
+  superHeroList = signal<SuperHeroModel[]>(superHeros);
+  filter = signal<string>('');
+  filteredList = computed(() => {
+    return this.superHeroList().filter(x => x.name.toLowerCase().indexOf(this.filter().toLowerCase()) > -1);
+  });
+  heroesNumber = computed(() => {
+    return this.superHeroList().length;
+  });
 
   onSearchOutput(searchString: string): void {
-    this.filteredList = this.superHeroList.filter(x => x.name.toLowerCase().indexOf(searchString.toLowerCase()) > -1);
+    this.filter.set(searchString);
   }
 
   addHero() {
-    this.superHeroList.push(
-      {
-        "id": 2,
-        "name": "Batman",
-        "slug": "70-batman",
-        "powerstats": {
-          "intelligence": 100,
-          "strength": 26,
-          "speed": 27,
-          "durability": 50,
-          "power": 47,
-          "combat": 100
-        },
-        "appearance": {
-          "gender": "Male",
-          "race": "Human",
-          "height": ["6'2", "188 cm"],
-          "weight": ["210 lb", "95 kg"],
-          "eyeColor": "blue",
-          "hairColor": "black"
-        },
-        "biography": {
-          "fullName": "Bruce Wayne",
-          "alterEgos": "No alter egos found.",
-          "aliases": ["Insider", "Matches Malone"],
-          "placeOfBirth": "Crest Hill, Bristol Township; Gotham County",
-          "firstAppearance": "Detective Comics #27",
-          "publisher": "DC Comics",
-          "alignment": "good"
-        },
-        "work": {
-          "occupation": "Businessman",
-          "base": "Batcave, Stately Wayne Manor, Gotham City; Hall of Justice, Justice League Watchtower"
-        },
-        "connections": {
-          "groupAffiliation": "Batman Family, Batman Incorporated, Justice League, Outsiders, Wayne Enterprises, Club of Heroes, formerly White Lantern Corps, Sinestro Corps",
-          "relatives": "Damian Wayne (son), Dick Grayson (adopted son), Tim Drake (adopted son), Jason Todd (adopted son), Cassandra Cain (adopted ward)\nMartha Wayne (mother, deceased), Thomas Wayne (father, deceased), Alfred Pennyworth (former guardian), Roderick Kane (grandfather, deceased), Elizabeth Kane (grandmother, deceased), Nathan Kane (uncle, deceased), Simon Hurt (ancestor), Wayne Family"
-        },
-        "images": {
-          "xs": "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/xs/70-batman.jpg",
-          "sm": "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/70-batman.jpg",
-          "md": "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/70-batman.jpg",
-          "lg": "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/70-batman.jpg"
-        }
+    const batMan = {
+      "id": 2,
+      "name": "Batman",
+      "slug": "70-batman",
+      "powerstats": {
+        "intelligence": 100,
+        "strength": 26,
+        "speed": 27,
+        "durability": 50,
+        "power": 47,
+        "combat": 100
+      },
+      "appearance": {
+        "gender": "Male",
+        "race": "Human",
+        "height": ["6'2", "188 cm"],
+        "weight": ["210 lb", "95 kg"],
+        "eyeColor": "blue",
+        "hairColor": "black"
+      },
+      "biography": {
+        "fullName": "Bruce Wayne",
+        "alterEgos": "No alter egos found.",
+        "aliases": ["Insider", "Matches Malone"],
+        "placeOfBirth": "Crest Hill, Bristol Township; Gotham County",
+        "firstAppearance": "Detective Comics #27",
+        "publisher": "DC Comics",
+        "alignment": "good"
+      },
+      "work": {
+        "occupation": "Businessman",
+        "base": "Batcave, Stately Wayne Manor, Gotham City; Hall of Justice, Justice League Watchtower"
+      },
+      "connections": {
+        "groupAffiliation": "Batman Family, Batman Incorporated, Justice League, Outsiders, Wayne Enterprises, Club of Heroes, formerly White Lantern Corps, Sinestro Corps",
+        "relatives": "Damian Wayne (son), Dick Grayson (adopted son), Tim Drake (adopted son), Jason Todd (adopted son), Cassandra Cain (adopted ward)\nMartha Wayne (mother, deceased), Thomas Wayne (father, deceased), Alfred Pennyworth (former guardian), Roderick Kane (grandfather, deceased), Elizabeth Kane (grandmother, deceased), Nathan Kane (uncle, deceased), Simon Hurt (ancestor), Wayne Family"
+      },
+      "images": {
+        "xs": "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/xs/70-batman.jpg",
+        "sm": "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/70-batman.jpg",
+        "md": "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/70-batman.jpg",
+        "lg": "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/70-batman.jpg"
+      }
 
-      });
-    //Adding a new Super Hero
-    // this.superHeroList = superHeros;
-    // this.herosNumber = superHeros.length;
+    }
+    this.superHeroList.update(values => {
+      return [...values, batMan];
+    });
   }
 
 }
